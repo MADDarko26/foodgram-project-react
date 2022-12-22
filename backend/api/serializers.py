2ee04180bbd15b7357b6ccb0026828082a8e6204
Макс Бирюков <mk-biryukov@mail.ru>
2022-12-22 23:51:17 +0300

    aftersecrew1

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -57,24 +57,22 @@ class RecipeSerializer(serializers.ModelSerializer):
                   'cooking_time')
 
     def validate(self, data):
-        ingredients = self.initial_data.get('ingredients')
-        if not ingredients:
+        list_ingr = [item['ingredient'] for item in data['ingredients']]
+        all_ingredients, distinct_ingredients = (
+            len(list_ingr), len(set(list_ingr)))
+
+        if all_ingredients != distinct_ingredients:
+            raise serializers.ValidationError(
+                {'error': 'Ингредиенты должны быть уникальными'}
+            )
+        if not distinct_ingredients:
             raise serializers.ValidationError({
                 'ingredients': 'Нужен хоть один ингридиент для рецепта'})
-        ingredient_list = []
-        for ingredient_item in ingredients:
-            ingredient = get_object_or_404(Ingredient,
-                                           id=ingredient_item['id'])
-            if ingredient in ingredient_list:
-                raise serializers.ValidationError('Ингридиенты должны '
-                                                  'быть уникальными')
-            ingredient_list.append(ingredient)
-            if int(ingredient_item['amount']) < 0:
-                raise serializers.ValidationError({
-                    'ingredients': ('Убедитесь, что значение количества '
-                                    'ингредиента больше 0')
+        if int(list_ingr['amount']) < 0:
+            raise serializers.ValidationError({
+                'ingredients': ('Убедитесь, что значение количества '
+                                'ингредиента больше 0')
                 })
-        data['ingredients'] = ingredients
         return data
 
     def create_ingredients(self, ingredients, recipe):
