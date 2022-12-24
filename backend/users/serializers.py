386c9ddf390ed973bcd202977b79c09f6125d99f
Макс Бирюков <mk-biryukov@mail.ru>
2022-12-24 17:45:20 +0300

    hw1

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -3,6 +3,8 @@ from djoser.serializers import UserCreateSerializer, UserSerializer
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
+from .models import Follow
+
 User = get_user_model()
 
 
@@ -26,10 +28,16 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
 
 class CustomUserSerializer(UserSerializer):
-    is_subscribed = serializers.BooleanField()
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = User
         fields = (
             'email', 'id', 'username', 'first_name', 'last_name',
             'is_subscribed')
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Follow.objects.filter(user=user, author=obj.id).exists()
