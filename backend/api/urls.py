79c5f766344cb646741ae5b554a4d57667886538
Макс Бирюков <mk-biryukov@mail.ru>
2022-12-24 16:06:56 +0300

    urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from api.views import IngredientsViewSet, RecipeViewSet, TagsViewSet
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from users.views import CustomUserViewSet
 
 app_name = 'api'
 
@@ -8,7 +9,10 @@ router = DefaultRouter()
 router.register('tags', TagsViewSet, basename='tags')
 router.register('ingredients', IngredientsViewSet, basename='ingredients')
 router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('users', CustomUserViewSet, basename='users')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,14 +1,14 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from users.views import CustomUserViewSet
+# from django.urls import include, path
+# from rest_framework.routers import DefaultRouter
+# from users.views import CustomUserViewSet
 
-app_name = 'api_users'
+# app_name = 'api_users'
 
-router = DefaultRouter()
-router.register('users', CustomUserViewSet, basename='users')
+# router = DefaultRouter()
+# router.register('users', CustomUserViewSet, basename='users')
 
-urlpatterns = [
-    path('', include(router.urls)),
-    path('', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-]
+# urlpatterns = [
+#     path('', include(router.urls)),
+#     path('', include('djoser.urls')),
+#     path('auth/', include('djoser.urls.authtoken')),
+# ]
